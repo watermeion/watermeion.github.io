b66071a715e672e38727c34550c3dd7eea91b798
guoyubao <guoyubao@qiyi.com>
2023-09-07 17:48:33 +0800

    Site updated: 2023-09-07 17:48:30

--- js/third-party/comments/gitalk.js
+++ js/third-party/comments/gitalk.js
@@ -14,7 +14,8 @@ document.addEventListener('page:loaded', () => {
         repo               : CONFIG.gitalk.repo,
         owner              : CONFIG.gitalk.github_id,
         admin              : [CONFIG.gitalk.admin_user],
-        id                 : CONFIG.gitalk.path_md5,
+        // id                 : CONFIG.gitalk.path_md5,
+        id                 : (location.pathname).split("/").pop().substring(0, 49),
         proxy              : CONFIG.gitalk.proxy,
         language           : CONFIG.gitalk.language || window.navigator.language,
         distractionFreeMode: CONFIG.gitalk.distraction_free_mode
